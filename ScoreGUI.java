104cebeb829f43ccea7959760f19297b03f861a0
samratyadav357 <54521305+samratyadav357@users.noreply.github.com>
2019-11-30 17:54:35 -0800

    Update ScoreGUI.java

--- ScoreGUI.java
+++ ScoreGUI.java
@@ -1,10 +1,12 @@
 package ScoreKeeper;
 
 import java.awt.FlowLayout;
+import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
+import javax.swing.JTextField;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 
@@ -19,15 +21,23 @@ public class ScoreGUI
 		JButton endbasketballgame = new JButton("End Basketball Game");
 		JButton endfootballgame = new JButton("End Football Game");
 		
-		JButton twopoints = new JButton("Two Pointer");
-		JButton freethrow = new JButton("Free Throw");
-		JButton threepoints = new JButton("Three Pointer");
+		JButton Team1two = new JButton("Team 1 Two Pointer");
+		JButton Team1freethrow = new JButton("Team 1 Free Throw");
+		JButton Team1three = new JButton("Team 1 Three Pointer");
 		
-		JButton touchdown = new JButton("Touchdown");
-		JButton extrakick = new JButton("Extra Kick");
-		JButton fieldgoal = new JButton("Field Goal");
+		JButton Team2two = new JButton("Team 2 Two Pointer");
+		JButton Team2freethrow = new JButton("Team 2 Free Throw");
+		JButton Team2three = new JButton("Team 2 Three Pointer");
 		
-		frame.setLayout(new FlowLayout());
+		JButton Team1touchdown = new JButton("Team 1 Touchdown");
+		JButton Team1extrakick = new JButton("Team 1 Safety");
+		JButton Team1fieldgoal = new JButton("Team 1 Field Goal");
+		
+		JButton Team2touchdown = new JButton("Team 2 Touchdown");
+		JButton Team2extrakick = new JButton("Team 2 Safety");
+		JButton Team2fieldgoal = new JButton("Team 2 Field Goal");
+		
+		frame.setLayout(new GridLayout(4,1));
 
 		frame.add(startbasketball);
 		frame.add(startfootball);
@@ -41,35 +51,170 @@ public class ScoreGUI
 				{
 					frame.remove(startfootball);
 					frame.add(endbasketballgame);
-					frame.add(twopoints);
-					frame.add(freethrow);
-					frame.add(threepoints);
 					
+					frame.add(Team1two);
+					frame.add(Team1freethrow);
+					frame.add(Team1three);
+					
+					frame.add(Team2two);
+					frame.add(Team2freethrow);
+					frame.add(Team2three);
+					
+					JLabel team1score = new JLabel();
+					JLabel team2score = new JLabel();
+					JLabel finalscore = new JLabel();
+					
+					Basketball ye = new Basketball("Team1", "Team2");
+					
+					ActionListener basketballaction = new ActionListener() 
+					{
+						public void actionPerformed(ActionEvent e) 
+						{
+							if (e.getSource() == endbasketballgame)
+							{	
+								finalscore.setText(ye.endGame());
+								
+							}
+							else if(e.getSource() == Team1two)
+							{
+								team1score.setText(ye.Team1AddTwo());
+							}
+							else if (e.getSource() == Team1freethrow)
+							{
+								team1score.setText(ye.Team1AddFT());
+							}
+							else if(e.getSource() == Team1three)
+							{
+								team1score.setText(ye.Team1AddThree());
+							}
+							else if (e.getSource() == Team2two)
+							{
+								team2score.setText(ye.Team2AddTwo());
+							}
+							else if(e.getSource() == Team2freethrow)
+							{
+								team2score.setText(ye.Team2AddFT());
+							}
+							else if (e.getSource() == Team2three)
+							{
+								team2score.setText(ye.Team2AddThree());
+							}
+						}
+					};
+					
+					
+					endbasketballgame.addActionListener(basketballaction);
+					
+					Team1two.addActionListener(basketballaction);
+					Team1freethrow.addActionListener(basketballaction);
+					Team1three.addActionListener(basketballaction);
+					
+					Team2two.addActionListener(basketballaction);
+					Team2freethrow.addActionListener(basketballaction);
+					Team2three.addActionListener(basketballaction);
+					
+					frame.add(team1score);
+					frame.add(team2score);
+					frame.add(finalscore);
 					frame.pack();
 					frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 					frame.setVisible(true);
 					
 				} 
-				else if( e.getSource() == startfootball)
-				{
-					frame.remove(startbasketball);
-					frame.add(endfootballgame);
-					frame.add(touchdown);
-					frame.add(extrakick);
-					frame.add(fieldgoal);
-					
-					frame.pack();
-					frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-					frame.setVisible(true);
-				}
 			}
 		};
 		
-		startbasketball.addActionListener(action);
-		startfootball.addActionListener(action);
+		ActionListener action2 = new ActionListener() 
+		{
+
+			public void actionPerformed(ActionEvent e) 
+			{
+				if (e.getSource() == startfootball)
+				{
+					frame.remove(startbasketball);
+					frame.add(endfootballgame);
 					
+					frame.add(Team1touchdown);
+					frame.add(Team1extrakick);
+					frame.add(Team1fieldgoal);
+					
+					frame.add(Team2touchdown);
+					frame.add(Team2extrakick);
+					frame.add(Team2fieldgoal);
+					
+					JLabel team1score = new JLabel();
+					JLabel team2score = new JLabel();
+					JLabel finalscore = new JLabel();
+					
+					Football ye = new Football("Team1", "Team2");
+					
+					ActionListener footballaction = new ActionListener() 
+					{
+						public void actionPerformed(ActionEvent e) 
+						{
+							if (e.getSource() == endfootballgame)
+							{	
+								finalscore.setText(ye.endGame());
+								
+							}
+							else if(e.getSource() == Team1touchdown)
+							{
+								team1score.setText(ye.Team1AddTouchdown());
+							}
+							else if (e.getSource() == Team1extrakick)
+							{
+								team1score.setText(ye.Team1AddSafety());
+							}
+							else if(e.getSource() == Team1fieldgoal)
+							{
+								team1score.setText(ye.Team1AddFG());
+							}
+							else if (e.getSource() == Team2touchdown)
+							{
+								team2score.setText(ye.Team2AddTouchdown());
+							}
+							else if(e.getSource() == Team2extrakick)
+							{
+								team2score.setText(ye.Team2AddSafety());
+							}
+							else if (e.getSource() == Team2fieldgoal)
+							{
+								team2score.setText(ye.Team2AddFG());
+							}
+						}
+					};
+					
+					
+					endfootballgame.addActionListener(footballaction);
+					
+					Team1touchdown.addActionListener(footballaction);
+					Team1extrakick.addActionListener(footballaction);
+					Team1fieldgoal.addActionListener(footballaction);
+					
+					Team2touchdown.addActionListener(footballaction);
+					Team2extrakick.addActionListener(footballaction);
+					Team2fieldgoal.addActionListener(footballaction);
+					
+					frame.add(team1score);
+					frame.add(team2score);
+					frame.add(finalscore);
+					frame.pack();
+					frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+					frame.setVisible(true);
+					
+				} 
+			}
+		};
+		
+		//end overall action listener
+		startbasketball.addActionListener(action);
+		startfootball.addActionListener(action2);
+		
 		frame.pack();
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setVisible(true);
+		
 	}
 }
+			
+
